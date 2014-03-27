9ed054f345d0857efa32e83d37e0ba4449c7d086
AlvinChiou <alvinhsian@gmail.com>
2014-03-28 01:59:13 +0800

    Some Problem: values[] return null...

--- src/Client/ClientInfo.java
+++ src/Client/ClientInfo.java
@@ -8,18 +8,28 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class ClientInfo extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         request.setCharacterEncoding("Big5");
         response.setContentType("text/html; charset=Big5");
         PrintWriter out = response.getWriter();
 
         String name = request.getParameter("chkColor");
-        String name_enc = new String(name.getBytes("ISO-8859-1"), "Big5");
-        String[] values = request.getParameterValues(name_enc);
-        if(values!=null){
-            for(int i = 0;i<values.length;i++){
-                out.println();
+        String name2 = new String(name.getBytes("ISO-8859-1"), "Big5");
+        String values[] = request.getParameterValues(name2);
+
+        if (values != null) {
+            out.println("<HTML>");
+            out.println("<BODY>");
+
+            for (int i = 0; i < values.length ; i++) {
+                out.println("[" + i + "]" + values[i]+"<br/>");
             }
+            out.println("</BODY>");
+            out.println("</HTML>");
+        } else {
+            out.println(values);
+            out.print("ªÅ­È!");
         }
 
 
